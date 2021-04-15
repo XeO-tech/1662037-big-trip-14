84927e277fc7dd649443df6c10ebb2acc61e9f3b
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-04-15 20:33:42 +0300

    minor fix

--- src/utils/render.js
+++ src/utils/render.js
@@ -30,7 +30,7 @@ export const replace = (newChild, oldChild) => {
   }
   const parent = oldChild.parentElement;
   if (parent === null || oldChild === null || newChild === null) {
-    throw new Error ('Can\'t replace unexisting elenents');
+    throw new Error ('Can\'t replace non existing elements');
   }
   parent.replaceChild(newChild, oldChild);
 };
