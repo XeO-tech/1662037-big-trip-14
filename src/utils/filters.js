2b6b53633e3d8858393852f0c6b8c6b6337064e8
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-05-03 16:41:21 +0300

    fix filters utils functions

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,16 +1,19 @@
 import { FilterTypes } from '../consts.js';
+import dayjs from 'dayjs';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+dayjs.extend(isSameOrAfter);
 
 export const filters = {
   [FilterTypes.FUTURE]: (events) => {
     return events.filter((eventInfo) => {
-      const currentDate = new Date(Date.now());
-      return eventInfo.date_from >= currentDate;
+      const currentDate = dayjs(new Date());
+      return dayjs(eventInfo.date_from).isSameOrAfter(currentDate, 'day');
     });
   },
   [FilterTypes.PAST]: (events) => {
     return events.filter((eventInfo) => {
-      const currentDate = new Date(Date.now());
-      return eventInfo.date_to < currentDate;
+      const currentDate = new dayjs(new Date());
+      return dayjs(eventInfo.date_to).isBefore(currentDate, 'day');
     });
   },
 };
